907ab42ea24149a73a4b31f84f0c499728567f0f
pilotvitalik <itvkip@yandex.ru>
2019-12-16 18:59:06 +0300

    v.4

--- src/main.js
+++ src/main.js
@@ -13,26 +13,6 @@ const router = new VueRouter({
 	mode: 'history',
 })
 
-const http = require('http')
-const fs = require('fs')
-const httpPort = 80
-
-http.createServer((req, res) => {
-  fs.readFile('index.htm', 'utf-8', (err, content) => {
-    if (err) {
-      console.log('Невозможно открыть файл "index.htm".')
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
-
-    res.end(content)
-  })
-}).listen(httpPort, () => {
-  console.log('Сервер запущен на: http://localhost:%s', httpPort)
-})
-
 new Vue({
   store,
   render: h => h(App),
